e7b1f9a9bbfd43d23ba1d509fa1ae2df656a3d63
Sakshyam11111 <sakshyamshrestha111@gmail.com>
2026-01-19 11:45:53 +0545

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="bg-white h-screen">
+    <Link href={'/sakshyam'} className="text-black ">
+      Hello
+    </Link>
+
+    </div>
+
+  );
+}
